979742e486bd1b748e80a210fedaa578bb585137
tianqitheodorejiang <47955281+tianqitheodorejiang@users.noreply.github.com>
2021-12-02 16:20:51 -0800

    Add files via upload

--- mutformer_model_code/run_classifier.py
+++ mutformer_model_code/run_classifier.py
@@ -411,7 +411,7 @@ def create_model(bert_config, model, is_training, input_ids, input_mask, segment
   #
   # If you want to use the token-level output, use model.get_sequence_output()
   # instead.
-  output_layer = model.get_sequence_output()
+  output_layer = model.get_pooled_output()
   print(output_layer.shape)
   if preds:
       pred_layer = tf.layers.dense(
