ac53f1f1a65cd609cfa4c9ba4bbd43ee470337fb
tianqitheodorejiang <47955281+tianqitheodorejiang@users.noreply.github.com>
2021-12-27 20:33:08 -0800

    Add files via upload

--- mutformer_model_code/manual predict.py
+++ mutformer_model_code/manual predict.py
@@ -21,9 +21,13 @@ import run_classifier as run_classifier
 init_checkpoint = "C:/Users/JiangQin/Documents/python/transformer_project_updated/models/pathgenicity prediction"
 init_checkpoint = tf.train.latest_checkpoint(init_checkpoint)
 
-test = "[CLS] B M V T E F I F L G L S D S Q E L Q T F L F M L F F V F Y G G I V F G N L L I V I T V V S D S H L H S P M Y F L L A N L S L I D L S L S S V T A P K M I T D F F S Q R K V I S F K G C L V Q I F L L H F F G G S E M V I L I A M G F D R Y I A I C K P L H Y T T I M C G N A C V G I M A V T W G I G F L H S V S Q L A F A V H L L F C G P N E V D S F Y C D L P R V I K L A C T D T Y R L D I M V I A N S G V L T V C S F V L L I I S Y T I I L M T I Q H R P L D K S S K A L S T L T A H I T V V L L F F G P C V F I Y A W [SEP] B M M T E F I F L G L S D S Q E L Q T F L F M L F F V F Y G G I V F G N L L I V I T V V S D S H L H S P M Y F L L A N L S L I D L S L S S V T A P K M I T D F F S Q R K V I S F K G C L V Q I F L L H F F G G S E M V I L I A M G F D R Y I A I C K P L H Y T T I M C G N A C V G I M A V T W G I G F L H S V S Q L A F A V H L L F C G P N E V D S F Y C D L P R V I K L A C T D T Y R L D I M V I A N S G V L T V C S F V L L I I S Y T I I L M T I Q H R P L D K S S K A L S T L T A H I T V V L L F F G P C V F I Y A W [SEP]"
+#test = "[CLS] B M V T E F I F L G L S D S Q E L Q T F L F M L F F V F Y G G I V F G N L L I V I T V V S D S H L H S P M Y F L L A N L S L I D L S L S S V T A P K M I T D F F S Q R K V I S F K G C L V Q I F L L H F F G G S E M V I L I A M G F D R Y I A I C K P L H Y T T I M C G N A C V G I M A V T W G I G F L H S V S Q L A F A V H L L F C G P N E V D S F Y C D L P R V I K L A C T D T Y R L D I M V I A N S G V L T V C S F V L L I I S Y T I I L M T I Q H R P L D K S S K A L S T L T A H I T V V L L F F G P C V F I Y A W [SEP] B M A T E F I F L G L S D S Q E L Q T F L F M L F F V F Y G G I V F G N L L I V I T V V S D S H L H S P M Y F L L A N L S L I D L S L S S V T A P K M I T D F F S Q R K V I S F K G C L V Q I F L L H F F G G S E M V I L I A M G F D R Y I A I C K P L H Y T T I M C G N A C V G I M A V T W G I G F L H S V S Q L A F A V H L L F C G P N E V D S F Y C D L P R V I K L A C T D T Y R L D I M V I A N S G V L T V C S F V L L I I S Y T I I L M T I Q H R P L D K S S K A L S T L T A H I T V V L L F F G P C V F I Y A W [SEP] [PAD]"
+#test = "[CLS] M L G S L A A L A A L A V I G D R P S L T H V V E W I D F E T L A L L F G M M I L V A I F S E T G F F D Y C A V K A Y R L S R G R V W A M I I M L C L I A A V L S A F L D N V T T M L L F T P V T I R L C E V L N L D P R Q V L I A E V I F T N I G G A A T A I G D P P N V I I V S N Q E L R K M G L D F A G F T A H M F I G I C L V L L V C F P L L R L L Y W N R K L Y N K E P S E I V E L K H E I H V W R L T A Q R I S P A S R E E T A V R R L L L G K V L A L E H L L A R R L H T F H R Q I S Q E D K N W E T N I Q E [SEP] M L G S L A A L A A L A V I G D R P S L T H V V E W I D F E T L A L L F G M M I L V A I F S E T G F F D Y C A V K A Y R L S R G R V W A M I I M L C L I A A V L S A F L D N V T T M L L F T P V T I R L C E V L N L D P R Q V L I A E V I F T N I G G A A T A I V D P P N V I I V S N Q E L R K M G L D F A G F T A H M F I G I C L V L L V C F P L L R L L Y W N R K L Y N K E P S E I V E L K H E I H V W R L T A Q R I S P A S R E E T A V R R L L L G K V L A L E H L L A R R L H T F H R Q I S Q E D K N W E T N I Q E [SEP] [PAD]"
+#test = "[CLS] T E Q A E A P C R G Q A C S A Q K A Q P V G T C P G E E W M I R K V K V E D E D Q E A E E E V E W P Q H L S L L P S P F P A P D L G H L A A A Y K L E P G A P G A L S G L A L S G W G P M P E K P Y G C G E C E R R F R D Q L T L R L H Q R L H R G E G P C A C P D C G R S F T Q R A H M L L H Q R S H R G E R P F P C S E C D K R F S K K A H L T R H L R T H T G E R P Y P C A E C G K R F S Q K I H L G S H Q K T H T G E R P F P C T E C E K R F R K K T H L I R H Q R I H T G E R P Y Q C A Q C A R S F T H K Q H L V [SEP] T E Q A E A P C R G Q A C S A Q K A Q P V G T C P G E E W M I R K V K V E D E D Q E A E E E V E W P Q H L S L L P S P F P A P D L G H L A A A Y K L E P G A P G A L S G L A L S G W G P M P E K P Y G C G E C E R R F R D Q L T L R L H Q R L H R G E G P C A C R D C G R S F T Q R A H M L L H Q R S H R G E R P F P C S E C D K R F S K K A H L T R H L R T H T G E R P Y P C A E C G K R F S Q K I H L G S H Q K T H T G E R P F P C T E C E K R F R K K T H L I R H Q R I H T G E R P Y Q C A Q C A R S F T H K Q H L V [SEP] [PAD]"
+test = "[CLS] A S E S R C Q Q G K T Q F G V G L R S G G E N H L W L L E G T P S L Q S C W A A C C Q D S A C H V F W W L E G M C I Q A D C S R P Q S C R A F R T H S S N S M L V F L K K F Q T A D D L G F L P E D D V P H L L G L G W N W A S W R Q S P P R A A L R P A V S S S D Q Q S L I R K L Q K R G S P S D V V T P I V T Q H S K V N D S N E L G G L T T S G S A E V H K A I T I S S P L T T D L T A E L S G G P K N V S V Q P E I S E G L A T T P S T Q Q V K S S E K T Q I A V P Q P V A P S Y S Y A T P T P Q A S F Q S T S A P [SEP] A S E S R C Q Q G K T Q F G V G L R S G G E N H L W L L E G T P S L Q S C W A A C C Q D S A C H V F W W L E G M C I Q A D C S R P Q S C R A F R T H S S N S M L V F L K K F Q T A D D L G F L P E D D V P H L L G L G W N W A S W R Q S P P R A A L R P A V S S C D Q Q S L I R K L Q K R G S P S D V V T P I V T Q H S K V N D S N E L G G L T T S G S A E V H K A I T I S S P L T T D L T A E L S G G P K N V S V Q P E I S E G L A T T P S T Q Q V K S S E K T Q I A V P Q P V A P S Y S Y A T P T P Q A S F Q S T S A P [SEP] [PAD]"
 
-
+file = "C:/Users/JiangQin/Documents/python/transformer_project_updated/data/final_finetuning_data/BERT finetuning no redundancies/MRPC/modified_bert_mrpc_512/test.tsv"
+#file = "C:/Users/JiangQin/Documents/python/transformer_project_updated/data/full_database_prediction/test_tiny.txt"
 vocab = \
 '''[PAD]
 [UNK]
@@ -53,27 +57,50 @@ C
 M
 W'''
 
-test = [vocab.index(x) for x in test.split(" ")]
-input_mask = [1 for x in test]
+tokenss = []
+segment_idss = []
+input_maskss = []
+labels = []
 
-segment_ids = []
-index = 0
-for tes in test:
-    segment_ids.append(index)
-    if tes==vocab.index("[SEP]"):
-       index+=1
+for line in open(file).read().split("\n"):
+    tokens = ["[CLS]"]
+    segment_ids = [0]
 
-print(segment_ids)
+    stuff = line.split("\t")
+
+    labels.append(stuff[0])
+
+    for char in stuff[3].split(" "):
+        tokens.append(char)
+        segment_ids.append(0)
+    tokens.append("[SEP]")
+    segment_ids.append(0)
+
+    for char in stuff[4].split(" "):
+        tokens.append(char)
+        segment_ids.append(1)
+    tokens.append("[SEP]")
+    segment_ids.append(1)
+
+    input_mask = [1 for x in tokens]
+    while len(tokens) < 512:
+        tokens.append("[PAD]")
+        segment_ids.append(0)
+        input_mask.append(0)
+    tokens = [vocab.split("\n").index(x) for x in tokens]
+    tokenss.append(tokens)
+    segment_idss.append(segment_ids)
+    input_maskss.append(input_mask)
 
 config = {
   "hidden_size": 768,
   "hidden_act": "gelu",
   "initializer_range": 0.02,
   "hidden_dropout_prob": 0.1,
-  "num_attention_heads": 8,
+  "num_attention_heads": 12,
   "type_vocab_size": 2,
   "max_position_embeddings": 1024,
-  "num_hidden_layers": 8,
+  "num_hidden_layers": 12,
   "intermediate_size": 3072,
   "attention_probs_dropout_prob": 0.1,
   "vocab_size": 27
@@ -83,53 +110,66 @@ num_labels = 2
 
 config = modeling.BertConfig.from_dict(config)
 
-print(tf.constant([test]))
+amt = 100
 
-model = modeling.BertModelModified(
-    config=config,
-    is_training=False,
-    input_ids=tf.constant([test]),
-    input_mask=tf.constant([input_mask]),
-    token_type_ids=tf.constant([segment_ids]),
-    use_one_hot_embeddings=True)
+with open("asdf.txt", "w+") as out:
+    for i in range(0,(len(tokenss)//100)+1):
+        print(i)
+        print("\n\n\n\n\n\n\n\n\n\n\n\n"+str(i/(len(tokenss)//100))+"\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        tokens = tf.constant(tokenss[amt*i:amt*(i+1)])
+        masks = tf.constant(input_maskss[amt*i:amt*(i+1)])
+        segments = tf.constant(segment_idss[amt*i:amt*(i+1)])
+
+        print(tokens,masks,segments)
 
 
-output_layer = model.get_pooled_output()
+        model = modeling.BertModelModified(
+            config=config,
+            is_training=False,
+            input_ids=tokens,
+            input_mask=masks,
+            token_type_ids=segments,
+            use_one_hot_embeddings=True)
 
-hidden_size = output_layer.shape[-1].value
 
-output_weights = tf.get_variable(
-    "output_weights", [num_labels, hidden_size],
-    initializer=tf.truncated_normal_initializer(stddev=0.02))
+        output_layer = model.get_pooled_output()
 
-output_bias = tf.get_variable(
-    "output_bias", [num_labels], initializer=tf.zeros_initializer())
+        hidden_size = output_layer.shape[-1].value
 
-with tf.variable_scope("loss"):
+        output_weights = tf.get_variable(
+            "output_weights", [num_labels, hidden_size],
+            initializer=tf.truncated_normal_initializer(stddev=0.02))
 
-    logits = tf.matmul(output_layer, output_weights, transpose_b=True)
-    logits = tf.nn.bias_add(logits, output_bias)
-    probabilities = tf.nn.softmax(logits, axis=-1)
+        output_bias = tf.get_variable(
+            "output_bias", [num_labels], initializer=tf.zeros_initializer())
 
-init = tf.global_variables_initializer()
+        with tf.variable_scope("loss"):
 
-tvars = tf.trainable_variables()
+            logits = tf.matmul(output_layer, output_weights, transpose_b=True)
+            logits = tf.nn.bias_add(logits, output_bias)
+            probabilities = tf.nn.softmax(logits, axis=-1)
 
-initialized_variable_names = {}
-scaffold_fn = None
-if init_checkpoint:
-    (assignment_map, initialized_variable_names) = \
-                   modeling.get_assignment_map_from_checkpoint(tvars, init_checkpoint)
-    in_chkpt = [var for var in tvars if var.name in initialized_variable_names]
-    not_in_chkpt = [var for var in tvars if var.name not in initialized_variable_names]
-    tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
 
-print(not_in_chkpt)
-print(init_checkpoint)
 
-with tf.Session() as sess:
-    #sess.run(init)
-    saver = tf.train.import_meta_graph("C:/Users/JiangQin/Documents/python/transformer_project_updated/models/pathgenicity prediction/model.ckpt-10000.meta")
-    saver.restore(sess, init_checkpoint)
-    print(sess.run(probabilities))
+        tvars = tf.trainable_variables()
 
+        initialized_variable_names = {}
+        scaffold_fn = None
+        if init_checkpoint:
+            (assignment_map, initialized_variable_names) = \
+                           modeling.get_assignment_map_from_checkpoint(tvars, init_checkpoint)
+            in_chkpt = [var for var in tvars if var.name in initialized_variable_names]
+            not_in_chkpt = [var for var in tvars if var.name not in initialized_variable_names]
+            tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
+
+        init = tf.global_variables_initializer()
+        print(not_in_chkpt)
+        print(init_checkpoint)
+
+        with tf.Session() as sess:
+            sess.run(init)
+            #saver = tf.train.import_meta_graph("C:/Users/JiangQin/Documents/python/transformer_project_updated/models/pathgenicity prediction/model.ckpt-10000.meta")
+            #saver.restore(sess, init_checkpoint)
+            asdf = sess.run(probabilities)
+            out.write("\n".join([str(x) for x in asdf])+"\n")
+        tf.compat.v1.reset_default_graph()
--- mutformer_model_code/modeling.py
+++ mutformer_model_code/modeling.py
@@ -107,7 +107,7 @@ class BertConfig(object):
     """Serializes this instance to a JSON string."""
     return json.dumps(self.to_dict(), indent=2, sort_keys=True) + "\n"
 
-def two_convs(x, filters, dropout, kernel_size=(3), padding="same", strides=1, pool_size = (2)):
+def two_convs(x, filters, dropout, kernel_size=(3), padding="same", strides=1):
     c = keras.layers.Conv1D(filters, kernel_size, padding=padding, strides=strides, activation="relu", input_shape = x.shape[1:], kernel_initializer='he_normal')(x)
     c = keras.layers.Dropout(dropout)(c)
     c = keras.layers.Conv1D(filters, kernel_size, padding=padding, strides=strides, activation="relu", input_shape = c.shape[1:], kernel_initializer='he_normal')(c)
@@ -270,28 +270,7 @@ class BertModel(object):
     return self.embedding_table
 
 class BertModelModified(object):
-  """BERT model ("Bidirectional Encoder Representations from Transformers").
 
-  Example usage:
-
-  ```python
-  # Already been converted into WordPiece token ids
-  input_ids = tf.constant([[31, 51, 99], [15, 5, 0]])
-  input_mask = tf.constant([[1, 1, 1], [1, 1, 0]])
-  token_type_ids = tf.constant([[0, 0, 1], [0, 2, 0]])
-
-  config = modeling.BertConfig(vocab_size=32000, hidden_size=512,
-    num_hidden_layers=8, num_attention_heads=6, intermediate_size=1024)
-
-  model = modeling.BertModel(config=config, is_training=True,
-    input_ids=input_ids, input_mask=input_mask, token_type_ids=token_type_ids)
-
-  label_embeddings = tf.get_variable(...)
-  pooled_output = model.get_pooled_output()
-  logits = tf.matmul(pooled_output, label_embeddings)
-  ...
-  ```
-  """
 
   def __init__(self,
                config,
@@ -301,23 +280,7 @@ class BertModelModified(object):
                token_type_ids=None,
                use_one_hot_embeddings=False,
                scope=None):
-    """Constructor for BertModel.
 
-    Args:
-      config: `BertConfig` instance.
-      is_training: bool. true for training model, false for eval model. Controls
-        whether dropout will be applied.
-      input_ids: int32 Tensor of shape [batch_size, seq_length].
-      input_mask: (optional) int32 Tensor of shape [batch_size, seq_length].
-      token_type_ids: (optional) int32 Tensor of shape [batch_size, seq_length].
-      use_one_hot_embeddings: (optional) bool. Whether to use one-hot word
-        embeddings or tf.embedding_lookup() for the word embeddings.
-      scope: (optional) variable scope. Defaults to "bert".
-
-    Raises:
-      ValueError: The config is invalid or one of the input tensor shapes
-        is invalid.
-    """
     config = copy.deepcopy(config)
     if not is_training:
       config.hidden_dropout_prob = 0.0
@@ -425,6 +388,125 @@ class BertModelModified(object):
   def get_embedding_table(self):
     return self.embedding_table
 
+
+class MutFormer(object):
+    def __init__(self,
+                 config,
+                 is_training,
+                 input_ids,
+                 input_mask=None,
+                 token_type_ids=None,
+                 use_one_hot_embeddings=False,
+                 scope=None):
+
+        config = copy.deepcopy(config)
+        if not is_training:
+            config.hidden_dropout_prob = 0.0
+            config.attention_probs_dropout_prob = 0.0
+
+        input_shape = get_shape_list(input_ids, expected_rank=2)
+        batch_size = input_shape[0]
+        seq_length = input_shape[1]
+
+        if input_mask is None:
+            input_mask = tf.ones(shape=[batch_size, seq_length], dtype=tf.int32)
+
+        if token_type_ids is None:
+            token_type_ids = tf.zeros(shape=[batch_size, seq_length], dtype=tf.int32)
+
+        with tf.variable_scope(scope, default_name="bert"):
+            with tf.variable_scope("embeddings"):
+                # Perform embedding lookup on the word ids.
+                (self.embedding_output, self.embedding_table) = embedding_lookup(
+                    input_ids=input_ids,
+                    vocab_size=config.vocab_size,
+                    embedding_size=config.hidden_size,
+                    initializer_range=config.initializer_range,
+                    word_embedding_name="word_embeddings",
+                    use_one_hot_embeddings=use_one_hot_embeddings)
+
+                # Add positional embeddings and token type embeddings, then layer
+                # normalize and perform dropout.
+                raw_embedding_output = embedding_postprocessor(
+                    input_tensor=self.embedding_output,
+                    use_token_type=True,
+                    token_type_ids=token_type_ids,
+                    token_type_vocab_size=config.type_vocab_size,
+                    token_type_embedding_name="token_type_embeddings",
+                    use_position_embeddings=True,
+                    position_embedding_name="position_embeddings",
+                    initializer_range=config.initializer_range,
+                    max_position_embeddings=config.max_position_embeddings,
+                    dropout_prob=config.hidden_dropout_prob)
+                conv1 = two_convs(raw_embedding_output, config.hidden_size, config.hidden_dropout_prob)
+                conv2 = two_convs(conv1, config.hidden_size, config.hidden_dropout_prob)
+                self.embedding_output = raw_embedding_output+conv1+conv2
+            with tf.variable_scope("encoder"):
+                # This converts a 2D mask of shape [batch_size, seq_length] to a 3D
+                # mask of shape [batch_size, seq_length, seq_length] which is used
+                # for the attention scores.
+                attention_mask = create_attention_mask_from_input_mask(
+                    input_ids, input_mask)
+
+                # Run the stacked transformer.
+                # `sequence_output` shape = [batch_size, seq_length, hidden_size].
+                self.all_encoder_layers = transformer_model(
+                    input_tensor=self.embedding_output,
+                    attention_mask=attention_mask,
+                    hidden_size=config.hidden_size,
+                    num_hidden_layers=config.num_hidden_layers,
+                    num_attention_heads=config.num_attention_heads,
+                    intermediate_size=config.intermediate_size,
+                    intermediate_act_fn=get_activation(config.hidden_act),
+                    hidden_dropout_prob=config.hidden_dropout_prob,
+                    attention_probs_dropout_prob=config.attention_probs_dropout_prob,
+                    initializer_range=config.initializer_range,
+                    do_return_all_layers=True)
+
+            self.sequence_output = self.all_encoder_layers[-1]
+            # The "pooler" converts the encoded sequence tensor of shape
+            # [batch_size, seq_length, hidden_size] to a tensor of shape
+            # [batch_size, hidden_size]. This is necessary for segment-level
+            # (or segment-pair-level) classification tasks where we need a fixed
+            # dimensional representation of the segment.
+            with tf.variable_scope("pooler"):
+                # We "pool" the model by simply taking the hidden state corresponding
+                # to the first token. We assume that this has been pre-trained
+                first_token_tensor = tf.squeeze(self.sequence_output[:, 0:1, :], axis=1)
+                self.pooled_output = tf.layers.dense(
+                    first_token_tensor,
+                    config.hidden_size,
+                    activation=tf.tanh,
+                    kernel_initializer=create_initializer(config.initializer_range))
+
+    def get_pooled_output(self):
+        return self.pooled_output
+
+    def get_sequence_output(self):
+        """Gets final hidden layer of encoder.
+
+        Returns:
+          float Tensor of shape [batch_size, seq_length, hidden_size] corresponding
+          to the final hidden of the transformer encoder.
+        """
+        return self.sequence_output
+
+    def get_all_encoder_layers(self):
+        return self.all_encoder_layers
+
+    def get_embedding_output(self):
+        """Gets output of the embedding lookup (i.e., input to the transformer).
+
+        Returns:
+          float Tensor of shape [batch_size, seq_length, hidden_size] corresponding
+          to the output of the embedding layer, after summing the word
+          embeddings with the positional embeddings and the token type embeddings,
+          then performing layer normalization. This is the input to the transformer.
+        """
+        return self.embedding_output
+
+    def get_embedding_table(self):
+        return self.embedding_table
 
 def gelu(x):
   """Gaussian Error Linear Unit.
@@ -685,6 +767,76 @@ def embedding_postprocessor(input_tensor,
 
   output = layer_norm_and_dropout(output, dropout_prob)
   return output
+
+def mutformer_postprocessor(input_tensor,
+                            use_token_type=False,
+                            token_type_ids=None,
+                            token_type_vocab_size=16,
+                            token_type_embedding_name="token_type_embeddings",
+                            use_position_embeddings=True,
+                            position_embedding_name="position_embeddings",
+                            initializer_range=0.02,
+                            max_position_embeddings=512,
+                            dropout_prob=0.1):
+
+  input_shape = get_shape_list(input_tensor, expected_rank=3)
+  batch_size = input_shape[0]
+  seq_length = input_shape[1]
+  width = input_shape[2]
+
+  output = input_tensor
+
+  if use_token_type:
+    if token_type_ids is None:
+      raise ValueError("`token_type_ids` must be specified if"
+                       "`use_token_type` is True.")
+    token_type_table = tf.get_variable(
+        name=token_type_embedding_name,
+        shape=[token_type_vocab_size, width],
+        initializer=create_initializer(initializer_range))
+    # This vocab will be small so we always do one-hot here, since it is always
+    # faster for a small vocabulary.
+    flat_token_type_ids = tf.reshape(token_type_ids, [-1])
+    one_hot_ids = tf.one_hot(flat_token_type_ids, depth=token_type_vocab_size)
+    token_type_embeddings = tf.matmul(one_hot_ids, token_type_table)
+    token_type_embeddings = tf.reshape(token_type_embeddings,
+                                       [batch_size, seq_length, width])
+    output += token_type_embeddings
+
+  if use_position_embeddings:
+    assert_op = tf.assert_less_equal(seq_length, max_position_embeddings)
+    with tf.control_dependencies([assert_op]):
+      full_position_embeddings = tf.get_variable(
+          name=position_embedding_name,
+          shape=[max_position_embeddings, width],
+          initializer=create_initializer(initializer_range))
+      # Since the position embedding table is a learned variable, we create it
+      # using a (long) sequence length `max_position_embeddings`. The actual
+      # sequence length might be shorter than this, for faster training of
+      # tasks that do not have long sequences.
+      #
+      # So `full_position_embeddings` is effectively an embedding table
+      # for position [0, 1, 2, ..., max_position_embeddings-1], and the current
+      # sequence has positions [0, 1, 2, ... seq_length-1], so we can just
+      # perform a slice.
+      position_embeddings = tf.slice(full_position_embeddings, [0, 0],
+                                     [seq_length, -1])
+      num_dims = len(output.shape.as_list())
+
+      # Only the last two dimensions are relevant (`seq_length` and `width`), so
+      # we broadcast among the first dimensions, which is typically just
+      # the batch size.
+      position_broadcast_shape = []
+      for _ in range(num_dims - 2):
+        position_broadcast_shape.append(1)
+      position_broadcast_shape.extend([seq_length, width])
+      position_embeddings = tf.reshape(position_embeddings,
+                                       position_broadcast_shape)
+      output += position_embeddings
+
+  output = layer_norm_and_dropout(output, dropout_prob)
+  return output
+
 
 
 def create_attention_mask_from_input_mask(from_tensor, to_mask):
