aa21b4a0f13dc49fe6127c4c0f906b24f8f71df7
TheoJiang <tianqitheodorejiang@gmail.com>
2022-02-05 11:50:57 -0800

    hopfully successfully implementing logs every n steps

--- mutformer_model_code/run_pretraining.py
+++ mutformer_model_code/run_pretraining.py
@@ -133,19 +133,18 @@ def model_fn_builder(bert_config, init_checkpoint, init_learning_rate,
             if logging_dir:
                 def host_call_fn(gs, loss, lr, acc, prec, recall, f1):
                     gs = gs[0]
+                    with tf.contrib.summary.create_file_writer(logging_dir).as_default():
+                        with tf.contrib.summary.always_record_summaries():
+                            tf.contrib.summary.scalar('loss', loss[0], step=gs)
+                            tf.contrib.summary.scalar('learning_rate', lr[0], step=gs)
+                            tf.contrib.summary.scalar('accuracy', acc[0], step=gs)
+                            tf.contrib.summary.scalar('precision', prec[0], step=gs)
+                            tf.contrib.summary.scalar('recall', recall[0], step=gs)
+                            tf.contrib.summary.scalar('multiclass_averaged_dice/f1', f1[0], step=gs)
                     def writing():
-                        with tf.contrib.summary.create_file_writer(logging_dir).as_default():
-                            with tf.contrib.summary.always_record_summaries():
-                                tf.contrib.summary.scalar('loss', loss[0], step=gs)
-                                tf.contrib.summary.scalar('learning_rate', lr[0], step=gs)
-                                tf.contrib.summary.scalar('accuracy', acc[0], step=gs)
-                                tf.contrib.summary.scalar('precision', prec[0], step=gs)
-                                tf.contrib.summary.scalar('recall', recall[0], step=gs)
-                                tf.contrib.summary.scalar('multiclass_averaged_dice/f1', f1[0], step=gs)
-
-                                return tf.contrib.summary.all_summary_ops()
+                        return tf.contrib.summary.all_summary_ops()
                     def not_writing():
-                        return []
+                        return [tf.constant(0) for _ in tf.contrib.summary.all_summary_ops()]
                     return tf.cond(tf.equal(tf.mod(tf.cast(gs,tf.int32),tf.constant(save_logs_every_n_steps)),tf.constant(0)),writing,not_writing)
                 gs_t = tf.reshape(global_step, [1])
                 loss_t = tf.reshape(total_loss, [1])
