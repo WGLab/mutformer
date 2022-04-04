da2c5eccf6ad0884e6bb784949a087268c93b387
TheoJiang <tianqitheodorejiang@gmail.com>
2022-04-03 23:02:35 -0700

    fixed freezing mechanism

--- mutformer_model_code/optimization.py
+++ mutformer_model_code/optimization.py
@@ -64,7 +64,8 @@ def create_optimizer(loss, init_lr, decay_per_step, num_warmup_steps, use_tpu, t
       tvars = tf.trainable_variables()
   grads = tf.gradients(loss, tvars)
   if grad_mask:
-      grads *= tf.cosntant(grad_mask)
+      grads = [g*grad_mask[n] for n,g in enumerate(grads)]
+
 
   # This is how the model was pre-trained.
   if clip:
