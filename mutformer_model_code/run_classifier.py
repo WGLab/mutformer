26e15e29e1ee37f4ea89bd88e254d3d790565eff
TheoJiang <tianqitheodorejiang@gmail.com>
2022-06-19 21:46:36 -0700

    changed data generation to ignore heading and trailing whitespaces

--- mutformer_model_code/run_classifier.py
+++ mutformer_model_code/run_classifier.py
@@ -97,14 +97,15 @@ class DataProcessor(object):
     with tf.gfile.Open(input_file, "r") as f:
       lines = []
       for n,line in enumerate(tqdm(f,"reading tsv")):
+        if read_range:
+            if n<read_range[0]: continue
+            elif n>=read_range[1]: break
         line = line.strip()
         if not line:
             read_range[1]+=1
             continue
         line = line.split("\t")
-        if read_range:
-            if n<read_range[0]: continue
-            elif n>=read_range[1]: break
+
         lines.append(line)
       return lines
 
