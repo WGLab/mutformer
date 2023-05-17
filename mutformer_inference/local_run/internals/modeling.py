dfc4d121b6f9f55b18b05ce75197c3601eba940d
tianqitheodorejiang <47955281+tianqitheodorejiang@users.noreply.github.com>
2023-05-17 00:45:36 -0400

    Add files via upload

--- mutformer_inference/local_run/internals/modeling.py
+++ mutformer_inference/local_run/internals/modeling.py
@@ -743,9 +743,7 @@ def dropout(input_tensor, dropout_prob):
 
 def layer_norm(input_tensor, name=None):
   """Run layer normalization on the last dimension of the tensor."""
-  return tf.contrib.layers.layer_norm(
-      inputs=input_tensor, begin_norm_axis=-1, begin_params_axis=-1, scope=name)
-
+  return tf.keras.layers.LayerNormalization(axis = -1)(input_tensor)
 
 def layer_norm_and_dropout(input_tensor, dropout_prob, name=None):
   """Runs layer normalization followed by dropout."""
